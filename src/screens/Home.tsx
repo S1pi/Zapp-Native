7fbb439932b661096e9f82e1d7fdd477d8d1f0be
Jim Pohjansaro <jimp@metropolia.fi>
2025-04-08 10:16:25 +0300

    map toCenter function  fixed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -118,7 +118,7 @@ const Home = () => {
           style={styles.map}
           showsUserLocation={true}
           userLocationPriority="high"
-          followsUserLocation={true}
+          followsUserLocation={false}
         >
           {cars
             .filter((car) => !car.reserved)
@@ -156,7 +156,7 @@ const Home = () => {
             // open sort menu
           }}
         >
-          <Ionicons name="funnel" size={20} color="white" />
+          <Ionicons name="funnel" size={30} color="white" />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
