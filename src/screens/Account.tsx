7ecf4e9d50f4014f07281264735609a1788e0970
lattexi <laurikarhu@outlook.com>
2025-04-18 23:49:56 +0300

    user update

--- src/screens/Account.tsx
+++ src/screens/Account.tsx
@@ -16,7 +16,6 @@ import CustomButton from '../components/CustomButton';
 import BackButton from '../components/BackButton';
 import CustomInput from '../components/CustomInput';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {UseUser} from '../hooks/apiHooks';
 import {UserUpdate} from '../../types/user';
 
 const Account = () => {
